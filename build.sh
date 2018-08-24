0a39aba0e92382caaba6987329fc1d9d4032074f
Timo Reitz <godsboss@gmx.de>
2018-08-24 21:16:01 +0200

    Build via Docker into ./dist.

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+cp index.html dist
+cp -r images dist
+
 cat export.scm | gimp -i -b -
-ls src/*.coffee | xargs coffee -j timining.coffee -c -b
-coffee -c init.coffee
+ls src/*.coffee | xargs coffee -j timining.coffee -c -b -o dist/timining.js
+coffee -o dist/init.js -c init.coffee
--- export.scm
+++ export.scm
@@ -1,3 +1,3 @@
 (let* ((image (car (gimp-file-load RUN-NONINTERACTIVE "./gfx.xcf" "gfx.xcf"))))
-	(file-png-save RUN-NONINTERACTIVE image (car (gimp-image-merge-visible-layers image EXPAND-AS-NECESSARY)) "./gfx.png" "gfx.png" 0 9 0 0 0 0 0)
+	(file-png-save RUN-NONINTERACTIVE image (car (gimp-image-merge-visible-layers image EXPAND-AS-NECESSARY)) "./dist/gfx.png" "gfx.png" 0 9 0 0 0 0 0)
 	(gimp-quit 0))
